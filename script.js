82d8f72c8fe784966678ab42c2772832dfdfd7b4
mikcheaela <170772727+mikcheaela@users.noreply.github.com>
2024-06-16 22:51:24 -0400

    Remove pseudocode from file
    Removed pseudocode from the start of the file for
    improved readability.

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-//when user inputs either rock, paper, or scissors
-//randomly generate a number between 1 and 3 inclusive
-//if number equals 1 (rock)
-    //if user input was rock
-        //return tie
-    //else if user input was scissors
-        //return win
-    //else 
-        //return loss
-//else if number equals 2 (paper)
-    //if user input was paper
-        //return tie
-    //else if user input was rock
-        //return win
-    //else 
-        //return loss
-//else
-    //if user input was scissors
-        //return tie
-    //else if user input was paper
-        //return win
-    //else
-        //return loss
-
 //randomly chooses rock paper or scissors
 function getComputerChoice(){
     let x = Math.floor((Math.random() * 3) + 1);
